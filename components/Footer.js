6e07f11f417b09efa0dab26c9883f5ed10ea2e6d
Rishabh Bhargava <lxnd77@gmail.com>
2022-12-13 00:00:43 +0530

    footer and services page

--- components/Footer.js
+++ components/Footer.js
@@ -3,28 +3,28 @@ import Link from './Link'
 export default function Footer() {
   return (
     <footer>
-      <div className="mt-16 max-h-[540px] w-full" data-scroll-section>
-        <hr className="m-auto w-[80%] border-t-2 border-red-700" />
-        <div className="mx-auto mx-auto mt-16 flex flex-row md:w-[70%]">
-          <div className="flex flex-col justify-around">
-            <img src="assets/logoblack.png" width="196px" />
-            <div className="flex flex-row">
-              <img src="assets/phone.png" />
+      <div className="w-full" data-scroll-section>
+        <hr className="m-8 mx-auto w-[80%] border-t-2 border-red-700" />
+        <div className="mx-auto flex flex-col md:w-[70%] md:flex-row ">
+          <div className="mx-auto flex max-w-[75%] flex-col justify-around gap-4 p-4">
+            <img src="assets/logoblack.png" width="150px" className="mb-4" />
+            <div className="flex flex-row items-center gap-2">
+              <img src="assets/phone.png" width="30px" />
               <p>+86-20-39232167 / 39232577 / 39232657 </p>
             </div>
-            <div className="flex flex-row">
-              <img src="assets/mail.png" />
+            <div className="flex flex-row items-center gap-2">
+              <img src="assets/mail.png" width="30px" />
               <p>info@ifield.com.cn</p>
             </div>
-            <div className="flex flex-row">
-              <img src="assets/location.png" />
+            <div className="flex flex-row items-center gap-2">
+              <img src="assets/location.png" width="30px" />
               <p>
                 5th Fl, Building A, Daxin Industrial Park, No.3 Kaifa Dong RD, Xishan Village,
                 Luopu, Panyu District, Guangzhou, Peoples Republic of China
               </p>
             </div>
           </div>
-          <div className="flex flex-col justify-around">
+          <div className="mx-auto flex flex-col justify-around gap-4 p-4">
             <h2>Subscribe to our newsletter to stay updated</h2>
             <div className="flex flex-row">
               <input className=" relative mx-0 mb-0 rounded" type="text"></input>
