02bdb47a45672e991e17598f50f5db26f48d419e
Rishabh Bhargava <lxnd77@gmail.com>
2022-12-14 13:23:18 +0530

    fix globe ref + hydration issue

--- components/GlobeSection.js
+++ components/GlobeSection.js
@@ -1,13 +1,19 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useRef, useEffect } from 'react'
 import { SizeMe } from 'react-sizeme'
 import GlobeModal from './GlobeModal'
+// import dynamic from 'next/dynamic'
+
+// const Globe = dynamic(import('react-globe.gl'), { ssr: false })
 let Globe = () => null
 if (typeof window !== 'undefined') Globe = require('react-globe.gl').default
-
 export default function GlobeSection() {
   const [mounted, setMounted] = useState(false)
   const [modalData, setModalData] = useState('nothing')
+  const [domLoaded, setDomLoaded] = useState(false)
 
+  useEffect(() => {
+    setDomLoaded(true)
+  }, [])
   const chinaAddr = {
     title: 'Corporate Location',
     line1: '5th Fl, Building A, Daxin Industrial Park',
@@ -18,6 +24,15 @@ export default function GlobeSection() {
     worktime1: ' Monday - Friday: 9:00 AM - 6:00 PM',
     worktime2: 'Saturday - Sunday: 9:00 AM - 12:00 PM',
   }
+
+  const globeRef = useRef(null)
+
+  // const init = () => {
+  //   if (globeRef.current) {
+  //     const globe = globeRef.current
+  //     globe.current.controls().enableZoom = false
+  //   }
+  // }
 
   const onRefChange = useCallback((node) => {
     if (node === null) {
@@ -154,18 +169,16 @@ export default function GlobeSection() {
       </div>
       <div>
         <div id="globeViz">
-          <SizeMe>
-            {({ size: { width } }) => (
-              <Globe
-                ref={onRefChange}
-                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                globeImageUrl="/assets/globe.png"
-                htmlElementsData={gData}
-                // width={width}
-                htmlElement={gHtmlElements}
-              />
-            )}
-          </SizeMe>
+          {domLoaded && (
+            <Globe
+              ref={onRefChange}
+              bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+              globeImageUrl="/assets/globe.png"
+              htmlElementsData={gData}
+              htmlElement={gHtmlElements}
+              // onGlobeReady={() => init()}
+            />
+          )}
         </div>
 
         <div className="text-center">
